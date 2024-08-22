4af8472fac6455a30b0a8609654aaadac1bf5d85
Jonathan Clem <j@jclem.me>
2024-08-22 17:23:45 -0400

    Emit an event on each loop of streamTools

--- src/generation.ts
+++ src/generation.ts
@@ -363,14 +363,37 @@ export const stream: {
   ) => provider.stream(params),
 );
 
+export interface StreamLoop {
+  readonly _tag: "StreamLoop";
+}
+
+const StreamLoop = Data.tagged<StreamLoop>("StreamLoop");
+
 /**
  * An event emitted during a stream using tool calls.
  */
-export type StreamToolsEvent = StreamEvent | ToolResult<unknown, unknown>;
+export type StreamToolsEvent =
+  | StreamEvent
+  | StreamLoop
+  | ToolResult<unknown, unknown>;
 
 export class MaxIterationsError extends Data.TaggedError(
   "MaxIterationsError",
 ) {}
+
+export type StreamToolsStream<FnDefns extends Readonly<ToolDefinitionAny[]>> =
+  Stream.Stream<
+    StreamToolsEvent,
+    | MaxIterationsError
+    | ParseError
+    | HttpClientError
+    | HttpBodyError
+    | UnknownException
+    | MissingParameterError
+    | ToolExecutionError<unknown>
+    | ToolDefinitionError<FnDefns[number]>,
+    Scope.Scope | ToolDefinitionContext<FnDefns[number]>
+  >;
 
 /**
  * Generate a stream of events from the LLM provider, invoking the defined tools
@@ -386,35 +409,11 @@ export class MaxIterationsError extends Data.TaggedError(
 export const streamTools: {
   <FnDefns extends Readonly<ToolDefinitionAny[]>>(
     params: StreamParams<FnDefns>,
-  ): (
-    provider: Provider,
-  ) => Stream.Stream<
-    StreamToolsEvent,
-    | MaxIterationsError
-    | ParseError
-    | HttpClientError
-    | HttpBodyError
-    | UnknownException
-    | MissingParameterError
-    | ToolExecutionError<unknown>
-    | ToolDefinitionError<FnDefns[number]>,
-    Scope.Scope | ToolDefinitionContext<FnDefns[number]>
-  >;
+  ): (provider: Provider) => StreamToolsStream<FnDefns>;
   <FnDefns extends Readonly<ToolDefinitionAny[]>>(
     provider: Provider,
     params: StreamParams<FnDefns>,
-  ): Stream.Stream<
-    StreamToolsEvent,
-    | MaxIterationsError
-    | ParseError
-    | HttpClientError
-    | HttpBodyError
-    | UnknownException
-    | MissingParameterError
-    | ToolExecutionError<unknown>
-    | ToolDefinitionError<FnDefns[number]>,
-    Scope.Scope | ToolDefinitionContext<FnDefns[number]>
-  >;
+  ): StreamToolsStream<FnDefns>;
 } = dual(
   2,
   <FnDefns extends Readonly<ToolDefinitionAny[]>>(
@@ -422,260 +421,233 @@ export const streamTools: {
     params: StreamParams<FnDefns>,
   ) => {
     return Stream.asyncEffect<
-      StreamToolsEvent,
-      | MaxIterationsError
-      | ParseError
-      | HttpClientError
-      | HttpBodyError
-      | UnknownException
-      | MissingParameterError
-      | ToolExecutionError<unknown>
-      | ToolDefinitionError<FnDefns[number]>,
-      Scope.Scope | ToolDefinitionContext<FnDefns[number]>
-    >(
-      (emit) =>
-        Effect.gen(function* () {
-          if (params.maxIterations === 0) {
-            return yield* Effect.fail(new MaxIterationsError());
-          }
+      Stream.Stream.Success<StreamToolsStream<FnDefns>>,
+      Stream.Stream.Error<StreamToolsStream<FnDefns>>,
+      Stream.Stream.Context<StreamToolsStream<FnDefns>>
+    >((emit) =>
+      Effect.gen(function* () {
+        if (params.maxIterations === 0) {
+          return yield* Effect.fail(new MaxIterationsError());
+        }
 
-          const single = (event: StreamToolsEvent) =>
-            Effect.promise(() => emit.single(event));
+        const single = (event: StreamToolsEvent) =>
+          Effect.promise(() => emit.single(event));
 
-          const end = () => Effect.promise(() => emit.end());
+        const end = () => Effect.promise(() => emit.end());
 
-          const fail = (
-            error:
-              | MaxIterationsError
-              | ParseError
-              | HttpClientError
-              | HttpBodyError
-              | UnknownException
-              | MissingParameterError
-              | ToolExecutionError<unknown>
-              | ToolDefinitionError<FnDefns[number]>,
-          ) => Effect.promise(() => emit.fail(error));
+        const fail = (error: Stream.Stream.Error<StreamToolsStream<FnDefns>>) =>
+          Effect.promise(() => emit.fail(error));
 
-          const fnCalls: {
-            id: string;
-            name: string;
-            args: string;
-            input: unknown;
-            fnDefn: FnDefns[number];
-          }[] = [];
+        const fnCalls: {
+          id: string;
+          name: string;
+          args: string;
+          input: unknown;
+          fnDefn: FnDefns[number];
+        }[] = [];
 
-          // On each event, we first check to see if it's a tool call and record
-          // it. Then, we emit the event.
-          const onStreamEvent = (event: StreamEvent) =>
-            pipe(
-              event,
-              Match.type<StreamEvent>().pipe(
-                Match.tag("ToolCall", (toolCall) =>
-                  Effect.gen(function* () {
-                    const fnFound = Array.findFirst(
-                      params.tools ?? [],
-                      (fn) => fn.name === toolCall.name,
+        // On each event, we first check to see if it's a tool call and record
+        // it. Then, we emit the event.
+        const onStreamEvent = (event: StreamEvent) =>
+          pipe(
+            event,
+            Match.type<StreamEvent>().pipe(
+              Match.tag("ToolCall", (toolCall) =>
+                Effect.gen(function* () {
+                  const fnFound = Array.findFirst(
+                    params.tools ?? [],
+                    (fn) => fn.name === toolCall.name,
+                  );
+
+                  if (Option.isNone(fnFound)) {
+                    return yield* Effect.fail(
+                      new UnknownToolCallError({ toolCall: toolCall }),
                     );
+                  }
 
-                    if (Option.isNone(fnFound)) {
-                      return yield* Effect.fail(
-                        new UnknownToolCallError({ toolCall: toolCall }),
-                      );
-                    }
-
-                    const input: unknown = yield* S.decodeUnknown(
-                      S.parseJson(fnFound.value.input),
-                    )(toolCall.arguments).pipe(
-                      Effect.catchTag("ParseError", (error) =>
-                        Effect.fail(
-                          new InvalidToolCallError({
-                            toolCall: toolCall,
-                            error,
-                          }),
-                        ),
+                  const input: unknown = yield* S.decodeUnknown(
+                    S.parseJson(fnFound.value.input),
+                  )(toolCall.arguments).pipe(
+                    Effect.catchTag("ParseError", (error) =>
+                      Effect.fail(
+                        new InvalidToolCallError({
+                          toolCall: toolCall,
+                          error,
+                        }),
                       ),
-                    );
+                    ),
+                  );
 
-                    fnCalls.push({
-                      id: toolCall.id,
-                      name: toolCall.name,
-                      args: toolCall.arguments,
-                      input,
-                      fnDefn: fnFound.value,
-                    });
+                  fnCalls.push({
+                    id: toolCall.id,
+                    name: toolCall.name,
+                    args: toolCall.arguments,
+                    input,
+                    fnDefn: fnFound.value,
+                  });
 
-                    return event;
-                  }),
-                ),
-                Match.orElse((event) => Effect.succeed(event)),
+                  return event;
+                }),
               ),
-              Effect.flatMap(single),
+              Match.orElse((event) => Effect.succeed(event)),
+            ),
+            Effect.flatMap(single),
+          );
+
+        const onInvalidToolCall = (
+          error: InvalidToolCallError | UnknownToolCallError,
+        ) =>
+          Effect.gen(function* () {
+            yield* single(
+              StreamEventEnum.InvalidToolCall({
+                id: error.toolCall.id,
+                name: error.toolCall.name,
+                arguments: error.toolCall.arguments,
+              }),
             );
 
-          const onInvalidToolCall = (
-            error: InvalidToolCallError | UnknownToolCallError,
-          ) =>
-            Effect.gen(function* () {
-              yield* single(
-                StreamEventEnum.InvalidToolCall({
-                  id: error.toolCall.id,
-                  name: error.toolCall.name,
-                  arguments: error.toolCall.arguments,
-                }),
-              );
+            const input = yield* S.decodeUnknown(S.parseJson())(
+              error.toolCall.arguments,
+            );
 
-              const input = yield* S.decodeUnknown(S.parseJson())(
-                error.toolCall.arguments,
-              );
-
-              const failedToolCall = new ToolUseEvent({
-                id: error.toolCall.id,
-                name: error.toolCall.name,
-                input,
-              });
-
-              const failedToolResult = new ToolResultErrorEvent({
-                id: error.toolCall.id,
-                name: error.toolCall.name,
-                result: error.message,
-              });
-
-              yield* streamTools(provider, {
-                ...params,
-                events: [...params.events, failedToolCall, failedToolResult],
-                maxIterations:
-                  params.maxIterations == null
-                    ? params.maxIterations
-                    : params.maxIterations - 1,
-              }).pipe(Stream.runForEach((event) => single(event)));
+            const failedToolCall = new ToolUseEvent({
+              id: error.toolCall.id,
+              name: error.toolCall.name,
+              input,
             });
 
-          const handleToolCalls = Effect.gen(function* () {
-            if (fnCalls.length === 0) {
-              return yield* end();
-            }
+            const failedToolResult = new ToolResultErrorEvent({
+              id: error.toolCall.id,
+              name: error.toolCall.name,
+              result: error.message,
+            });
 
-            const newEvents: Extract<
-              ThreadEvent,
-              {
-                _tag:
-                  | "ToolUseEvent"
-                  | "ToolResultSuccessEvent"
-                  | "ToolResultErrorEvent";
-              }
-            >[] = [];
-
-            for (const fnCall of fnCalls) {
-              const toolCallEvent = new ToolUseEvent({
-                id: fnCall.id,
-                name: fnCall.name,
-                input: fnCall.input,
-              });
-
-              const output: Either.Either<
-                any,
-                ToolError<any>
-              > = yield* fnCall.fnDefn.effect(fnCall.id, fnCall.input).pipe(
-                Effect.map(Either.right),
-                Effect.catchTag("ToolError", (err) =>
-                  Effect.succeed(Either.left(err)),
-                ),
-                Effect.catchAll((error: unknown) =>
-                  Effect.fail(
-                    error instanceof HaltToolLoopError
-                      ? error
-                      : new ToolExecutionError({ id: fnCall.id, error }),
-                  ),
-                ),
-              );
-
-              newEvents.push(toolCallEvent);
-
-              if (Either.isRight(output)) {
-                yield* single(
-                  ToolResultEnum.ToolResultSuccess({
-                    id: fnCall.id,
-                    name: fnCall.name,
-                    result: output.right as unknown,
-                  }),
-                );
-
-                newEvents.push(
-                  new ToolResultSuccessEvent({
-                    id: fnCall.id,
-                    name: fnCall.name,
-                    result: output.right as unknown,
-                  }),
-                );
-              } else {
-                yield* single(
-                  ToolResultEnum.ToolResultError({
-                    id: fnCall.id,
-                    name: fnCall.name,
-                    result: output.left.payload as unknown,
-                  }),
-                );
-
-                newEvents.push(
-                  new ToolResultErrorEvent({
-                    id: fnCall.id,
-                    name: fnCall.name,
-                    result: output.left.payload as unknown,
-                  }),
-                );
-              }
-            }
-
-            if (newEvents.length === 0) {
-              return;
-            }
-
-            const newParams = {
+            yield* streamTools(provider, {
               ...params,
-              events: [...params.events, ...newEvents],
+              events: [...params.events, failedToolCall, failedToolResult],
               maxIterations:
                 params.maxIterations == null
                   ? params.maxIterations
                   : params.maxIterations - 1,
-            };
-
-            yield* streamTools(provider, newParams).pipe(
-              Stream.runForEach((e) => single(e)),
-            );
+            }).pipe(Stream.runForEach((event) => single(event)));
           });
 
-          yield* provider.stream(params).pipe(
-            Stream.runForEach(onStreamEvent),
-            Effect.catchIf(
-              (err): err is InvalidToolCallError =>
-                err instanceof InvalidToolCallError,
-              (error) => onInvalidToolCall(error),
-            ),
-            Effect.catchIf(
-              (err): err is UnknownToolCallError =>
-                err instanceof UnknownToolCallError,
-              (error) => onInvalidToolCall(error),
-            ),
-            Effect.andThen(() => handleToolCalls),
-            Effect.catchTag("HaltToolLoopError", () => end()),
-            Effect.catchAll((error) => fail(error)),
-            Effect.catchAllDefect((defect) =>
-              fail(new UnknownException(defect)),
-            ),
-            Effect.andThen(() => end()),
-            Effect.forkScoped,
+        const handleToolCalls = Effect.gen(function* () {
+          if (fnCalls.length === 0) {
+            return yield* end();
+          }
+
+          const newEvents: Extract<
+            ThreadEvent,
+            {
+              _tag:
+                | "ToolUseEvent"
+                | "ToolResultSuccessEvent"
+                | "ToolResultErrorEvent";
+            }
+          >[] = [];
+
+          for (const fnCall of fnCalls) {
+            const toolCallEvent = new ToolUseEvent({
+              id: fnCall.id,
+              name: fnCall.name,
+              input: fnCall.input,
+            });
+
+            const output: Either.Either<
+              any,
+              ToolError<any>
+            > = yield* fnCall.fnDefn.effect(fnCall.id, fnCall.input).pipe(
+              Effect.map(Either.right),
+              Effect.catchTag("ToolError", (err) =>
+                Effect.succeed(Either.left(err)),
+              ),
+              Effect.catchAll((error: unknown) =>
+                Effect.fail(
+                  error instanceof HaltToolLoopError
+                    ? error
+                    : new ToolExecutionError({ id: fnCall.id, error }),
+                ),
+              ),
+            );
+
+            newEvents.push(toolCallEvent);
+
+            if (Either.isRight(output)) {
+              yield* single(
+                ToolResultEnum.ToolResultSuccess({
+                  id: fnCall.id,
+                  name: fnCall.name,
+                  result: output.right as unknown,
+                }),
+              );
+
+              newEvents.push(
+                new ToolResultSuccessEvent({
+                  id: fnCall.id,
+                  name: fnCall.name,
+                  result: output.right as unknown,
+                }),
+              );
+            } else {
+              yield* single(
+                ToolResultEnum.ToolResultError({
+                  id: fnCall.id,
+                  name: fnCall.name,
+                  result: output.left.payload as unknown,
+                }),
+              );
+
+              newEvents.push(
+                new ToolResultErrorEvent({
+                  id: fnCall.id,
+                  name: fnCall.name,
+                  result: output.left.payload as unknown,
+                }),
+              );
+            }
+          }
+
+          if (newEvents.length === 0) {
+            return;
+          }
+
+          const newParams = {
+            ...params,
+            events: [...params.events, ...newEvents],
+            maxIterations:
+              params.maxIterations == null
+                ? params.maxIterations
+                : params.maxIterations - 1,
+          };
+
+          yield* streamTools(provider, newParams).pipe(
+            Stream.runForEach((e) => single(e)),
           );
-        }) as Effect.Effect<
-          void,
-          | MaxIterationsError
-          | ParseError
-          | HttpClientError
-          | HttpBodyError
-          | UnknownException
-          | ToolExecutionError<unknown>
-          | ToolDefinitionError<FnDefns[number]>,
-          Scope.Scope | ToolDefinitionContext<FnDefns[number]>
-        >,
+        });
+
+        yield* single(StreamLoop());
+
+        yield* provider.stream(params).pipe(
+          Stream.runForEach(onStreamEvent),
+          Effect.catchIf(
+            (err): err is InvalidToolCallError =>
+              err instanceof InvalidToolCallError,
+            (error) => onInvalidToolCall(error),
+          ),
+          Effect.catchIf(
+            (err): err is UnknownToolCallError =>
+              err instanceof UnknownToolCallError,
+            (error) => onInvalidToolCall(error),
+          ),
+          Effect.andThen(() => handleToolCalls),
+          Effect.catchTag("HaltToolLoopError", () => end()),
+          Effect.catchAll((error) => fail(error)),
+          Effect.catchAllDefect((defect) => fail(new UnknownException(defect))),
+          Effect.andThen(() => end()),
+          Effect.fork,
+        );
+      }),
     );
   },
 );
